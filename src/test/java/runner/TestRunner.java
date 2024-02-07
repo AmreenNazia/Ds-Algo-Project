58fbb732964fd68ec548d9ff8f3488567d3e1de2
Amreen <amreennazia1989@gmail.com>
2024-02-07 10:32:43 -0600

    Updated Extent.properties,pom.xml,testng.xml,testrunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,28 +1,36 @@
 package runner;
 
-import org.junit.runner.RunWith;
+import org.testng.annotations.BeforeTest;
 
- 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
- 
- 
-@RunWith(Cucumber.class) 
-@CucumberOptions(features="src/test/resources/Feature",glue = {"StepDefinition", "AppHooks"},
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import utility.ConfigReader;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@Test
+@CucumberOptions(features="src/test/resources/Feature/Tree.feature",glue = {"StepDefinition","AppHooks"},
 monochrome = true,
 plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		"html:target/HtmlReport/htmlreport.html"}
  
- 
- 
-		 
-		 
-)
-public class TestRunner {
-	
-	
-	 
+ )
+public class TestRunner extends AbstractTestNGCucumberTests {
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
 	}
+	@BeforeTest
+	@Parameters({ "browser" })
+	public void defineBrowser(String browser) throws Throwable {
+		ConfigReader.setBrowserType(browser);
+
+	}
+}
 
 	
  
