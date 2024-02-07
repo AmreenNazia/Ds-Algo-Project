1eaf334800b5b561b7a9f1919c71e949494874ae
Amreen <amreennazia1989@gmail.com>
2024-02-07 11:36:13 -0600

    Changed Application Hooks, BaseTest,TestRunner

--- src/test/java/AppHooks/ApplicationHooks.java
+++ src/test/java/AppHooks/ApplicationHooks.java
@@ -1,14 +1,19 @@
 package AppHooks;
 
- 
+
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeMethod;
+ 
+ 
+
 import java.io.File;
 
 
- 
+
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+ 
 import java.util.Date;
 import java.util.Properties;
 
@@ -21,65 +26,61 @@ import org.openqa.selenium.WebDriver;
 import Driverfactory.BaseTest;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
- 
+
 import io.cucumber.java.Scenario;
- 
+
 import utility.ConfigReader;
 
+
 public class ApplicationHooks {
-	
+
 	private static  BaseTest basetest;
-	private static WebDriver driver;
+	private   WebDriver driver;
 	private static ConfigReader configReader;
-	  static Properties prop;
-	
-	 
-	 
-	 
+	static Properties prop;
+
+
 	 
 	@BeforeMethod
 	@Before(order = 0)
 	public void get_property() throws IOException {
 		configReader = new ConfigReader();
 		prop = configReader.init_prop();
-		
-		
+
 	}
-	
-	 
-	 
-	 
+
+
 	 
 	@BeforeMethod
 	@Before(order = 1)
 	public void launchBrowser() throws Throwable {
-		String browser =  prop.getProperty("browser");
+		String browser = ConfigReader.getBrowserType();
 		String url = prop.getProperty("testurl");
 		basetest = new BaseTest();
 		driver = basetest.init_driver(browser);
+		System.out.println(driver);
 		driver.navigate().to(url);
 	}
-	
+
+
 	 
-	 
-	@AfterMethod
+	@AfterMethod  
 	@After(order = 0)
 	public void quitBrowser() {
+
 		driver.quit();
 	}
-	 
-	 
-	 
-	@AfterMethod
+
+	@AfterMethod 
 	@After(order = 1)
 	public void tearDown(Scenario scenario) throws IOException {
 		if(scenario.isFailed()) {
 			Date d = new Date();
 			System.out.println(d.toString());
-			
+
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HHmmss");
 			File sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(sourcePath, new File((System.getProperty("user.dir")+"/ScreenShorts")+"/image"+sdf.format(d)+".png"));
+			FileUtils.copyFile(sourcePath, new File((System.getProperty("user.dir")+"/ScreenShots")+"/image"+sdf.format(d)+".png"));
 		}
 	}
 
--- src/test/java/Driverfactory/BaseTest.java
+++ src/test/java/Driverfactory/BaseTest.java
@@ -1,57 +1,60 @@
 package Driverfactory;
- 
-import java.time.Duration;
 
-import org.bouncycastle.oer.its.etsi102941.Url;
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.safari.SafariDriver;
 
- 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class BaseTest  {
 
 	public static WebDriver driver;
-	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-	public static ThreadLocal<Url> url = new ThreadLocal<>();
-	 
-	
+	static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
 	public WebDriver init_driver(String browser) {
+
+	 
+			if(browser.equalsIgnoreCase("chrome"))
+			{
+
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("headless");
+				tlDriver.set(new ChromeDriver(options)); 
+			}
+			else if(browser.equalsIgnoreCase("firefox"))
+			{
+				WebDriverManager.firefoxdriver().setup();
+				tlDriver.set(new FirefoxDriver()); 
+			}
+			else if(browser.equalsIgnoreCase("safari"))
+			{
+				WebDriverManager.safaridriver().setup();
+				tlDriver.set(new SafariDriver()); 
+			}
+			else if(browser.equalsIgnoreCase("edge")) {
+				WebDriverManager.edgedriver().setup();
+				EdgeOptions options = new EdgeOptions();
+				options.addArguments("headless");
+				tlDriver.set(new EdgeDriver(options)); 
+			}
+			else {
+				System.out.println("please pass the correct broswer " + browser);
+
+			}
 		
-	if(browser.equalsIgnoreCase("chrome"))
-	{
-		 
-		WebDriverManager.chromedriver().setup();
-		tlDriver.set(new ChromeDriver()); 
-	}
-	else if(browser.equalsIgnoreCase("firefox"))
-	{
-		WebDriverManager.firefoxdriver().setup();
-		tlDriver.set(new FirefoxDriver());
-	}
-	else if(browser.equalsIgnoreCase("safari"))
-	{
-		WebDriverManager.safaridriver().setup();
-		tlDriver.set(new SafariDriver());
-		
-	}
-	else if(browser.equalsIgnoreCase("edge")) {
-		WebDriverManager.edgedriver().setup();
-		tlDriver.set(new EdgeDriver());
-	}
-	else {
-		 System.out.println("please pass the correct broswer " + browser);
-		 
-	}
-	 getDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-	 getDriver().manage().window().maximize();
-	
-	return getDriver();
-	
- 
+		getDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		getDriver().manage().window().maximize();
+
+		return getDriver();
+
+
 	}
 	public static synchronized WebDriver getDriver() {
 		return tlDriver.get();
