1f50052b455c66015e19c3bad3a7b84af7a58a7b
AmreenNazia <156974279+AmreenNazia@users.noreply.github.com>
2024-02-11 21:20:50 -0600

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import utility.ConfigReader;
  
-@CucumberOptions(features={"src/test/resources/Feature",
+@CucumberOptions(features={"src/test/resources/Feature"},
 		  glue = {"StepDefinition","AppHooks"},
 monochrome = true,
 plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
